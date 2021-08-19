aad672c59e6fc25eb01589899130e4281205a726
Arafat Hasan <opendoor.arafat@gmail.com>
2021-08-19 22:37:19 +0600

    Simple JWT implemented

--- main.go
+++ main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"math/rand"
 	"net/http"
+	"os"
 	"strconv"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
@@ -26,26 +29,17 @@ type Author struct {
 
 var books []Book
 
-func main() {
-	router := mux.NewRouter().StrictSlash(true)
-
-	// Hardcoded data - @todo: add database
-	books = append(books, Book{ID: "1", Isbn: "438227", Title: "Book One", Author: &Author{Firstname: "John", Lastname: "Doe"}})
-	books = append(books, Book{ID: "2", Isbn: "454555", Title: "Book Two", Author: &Author{Firstname: "Steve", Lastname: "Smith"}})
-
-	router.HandleFunc("/books", getBooks).Methods("GET")
-	router.HandleFunc("/books/{id}", getBook).Methods("GET")
-	router.HandleFunc("/books", addBook).Methods("POST")
-	router.HandleFunc("/books/{id}", updateBook).Methods("PUT")
-	router.HandleFunc("/books/{id}", deleteBook).Methods("DELETE")
-
-	log.Fatal(http.ListenAndServe(":8000", router))
+func homePage(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	log.Println("Endpoint Hit: homePage")
 }
 
 func getBooks(w http.ResponseWriter, r *http.Request) {
 	log.Println("getBooks called")
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(books)
+	w.WriteHeader(http.StatusOK)
 }
 
 func getBook(w http.ResponseWriter, r *http.Request) {
@@ -56,14 +50,15 @@ func getBook(w http.ResponseWriter, r *http.Request) {
 	for _, item := range books {
 		if item.ID == params["id"] {
 			json.NewEncoder(w).Encode((item))
+			w.WriteHeader(http.StatusOK)
 			return
 		}
 	}
-	json.NewEncoder(w).Encode(&Book{})
-	// w.WriteHeader(http.StatusNotFound)
+	// json.NewEncoder(w).Encode(&Book{})
+	w.WriteHeader(http.StatusNotFound)
 }
 
-func addBook(w http.ResponseWriter, r *http.Request) {
+var addBookHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 	log.Println("addBook called")
 	w.Header().Set("Content-Type", "application/json")
 	var book Book
@@ -71,9 +66,10 @@ func addBook(w http.ResponseWriter, r *http.Request) {
 	book.ID = strconv.Itoa(rand.Intn(1000000)) // mock data - not safe
 	books = append(books, book)
 	json.NewEncoder(w).Encode(book)
-}
+	w.WriteHeader(http.StatusOK)
+})
 
-func updateBook(w http.ResponseWriter, r *http.Request) {
+var updateBookHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 	log.Println("updateBook called")
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
@@ -85,12 +81,13 @@ func updateBook(w http.ResponseWriter, r *http.Request) {
 			book.ID = params["id"]
 			books = append(books, book)
 			json.NewEncoder(w).Encode(book)
+			w.WriteHeader(http.StatusOK)
 			return
 		}
 	}
-}
+})
 
-func deleteBook(w http.ResponseWriter, r *http.Request) {
+var deleteBookHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 	log.Println("deleteBook called")
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
@@ -101,4 +98,30 @@ func deleteBook(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 	json.NewEncoder(w).Encode(books)
+	w.WriteHeader(http.StatusOK)
+})
+
+func handleRequests() {
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.PathPrefix("/static").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+
+	router.HandleFunc("/authenticate", authenticate)
+
+	router.HandleFunc("/", homePage)
+	router.HandleFunc("/books", getBooks).Methods("GET")
+	router.HandleFunc("/books/{id}", getBook).Methods("GET")
+	router.Handle("/books", authMiddleware(addBookHandler)).Methods("POST")
+	router.Handle("/books/{id}", authMiddleware(updateBookHandler)).Methods("PUT")
+	router.Handle("/books/{id}", authMiddleware(deleteBookHandler)).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8000", handlers.LoggingHandler(os.Stdout, router)))
+}
+
+func main() {
+
+	// Hardcoded data - @todo: add database
+	books = append(books, Book{ID: "1", Isbn: "438227", Title: "Book One", Author: &Author{Firstname: "John", Lastname: "Doe"}})
+	books = append(books, Book{ID: "2", Isbn: "454555", Title: "Book Two", Author: &Author{Firstname: "Steve", Lastname: "Smith"}})
+	handleRequests()
 }
